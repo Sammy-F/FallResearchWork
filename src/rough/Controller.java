3eab0a88170f8cc6f966bf70fe3594c10281306c
Sammy-F <sfritsch@macalester.edu>
2018-07-26 18:44:41 -0500

    moved modifiable object handling into Controller

--- src/rough/Controller.java
+++ src/rough/Controller.java
@@ -2,10 +2,56 @@ package rough;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
-import javafx.scene.control.ListView;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.*;
+import javafx.scene.layout.HBox;
+import java.net.URL;
+import java.util.*;
 
-public class Controller {
+public class Controller implements Initializable {
 
-    public Controller() {}
+    // MODELS
+    private ObservableList<DataEntry> entries;
+    private Dataset mData;
+    private ListView<DataEntry> dataList;
+
+    // VIEWS
+    @FXML
+    private HBox mBox;
+
+    // CONSTANTS
+    static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
+    static final String DB_URL = "jdbc:mysql://localhost/EMP";
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        Map<String, Object> characteristics = new HashMap<String, Object>();
+
+        characteristics.put("Hello", "World");
+        characteristics.put("Hello", "World2");
+        characteristics.put("Hellop", "World");
+        characteristics.put("Hello2", "World2");
+
+        mData = new Dataset();
+        mData.addEntry(new DataEntry(0, true, characteristics));
+        mData.addEntry(new DataEntry(0, true));
+        mData.addEntry(new DataEntry(0, true, characteristics));
+        mData.addEntry(new DataEntry(0, true));
+        mData.addEntry(new DataEntry(0, true));
+        entries = FXCollections.observableArrayList(mData.getData());
+        dataList = new ListView<DataEntry>(entries);
+        dataList.setPrefWidth(800);
+
+        System.out.println(mBox);
+
+        mBox.getChildren().add(dataList);
+    }
 
 }
+
+
+
--- src/rough/Main.java
+++ src/rough/Main.java
@@ -4,10 +4,12 @@ import javafx.application.Application;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.ListView;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 
 import java.util.HashMap;
@@ -15,37 +17,16 @@ import java.util.Map;
 
 public class Main extends Application {
 
-    ObservableList<DataEntry> entries;
-    Dataset mData;
-    ListView<DataEntry> dataList;
-
     @Override
     public void start(Stage primaryStage) throws Exception{
 
-        Map<String, Object> characteristics = new HashMap<String, Object>();
-
-        characteristics.put("Hello", "World");
-        characteristics.put("Hello", "World2");
-        characteristics.put("Hellop", "World");
-        characteristics.put("Hello2", "World2");
-
-        mData = new Dataset();
-        mData.addEntry(new DataEntry(0, true, characteristics));
-        mData.addEntry(new DataEntry(0, true));
-        mData.addEntry(new DataEntry(0, true, characteristics));
-        mData.addEntry(new DataEntry(0, true));
-        mData.addEntry(new DataEntry(0, true));
-        entries = FXCollections.observableArrayList(mData.getData());
-        dataList = new ListView<DataEntry>(entries);
-        dataList.setPrefWidth(800);
-
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        ((GridPane) root).getChildren().add(dataList);
+        Node mBox = ((GridPane) root).getChildren().get(0);
+        mBox = new HBox();
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 800, 275));
         primaryStage.show();
     }
-
 
     public static void main(String[] args) {
         launch(args);
